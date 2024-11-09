829f8b2e0c9671e75262a4b22fffec72aad61474
Lequanghuy19204 <lequanghuy19204@gmail.com>
2024-11-09 23:25:19 +0700

    update

--- app/models.py
+++ app/models.py
@@ -25,7 +25,7 @@ class User(UserMixin,db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    role = db.Column(db.String(10))
+    role = db.Column(db.Integer, default = 0)
 
     def repr(self):
         return '<User {}>'.format(self.username)
